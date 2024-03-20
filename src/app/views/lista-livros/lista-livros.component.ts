f1eacab4378a8ccecc51f274c1dff501b4be809e
jorge-rufino <jorge.rufino1986@gmail.com>
2024-03-20 10:50:43 -0300

    Usando debounceTime para gerar um delay

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,10 +1,11 @@
 import { FormControl } from '@angular/forms';
 import { Item } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { switchMap, map, filter } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { switchMap, map, filter, debounceTime, tap } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 500; //Tempo dem milisegundos
 
 @Component({
   selector: 'app-lista-livros',
@@ -20,9 +21,12 @@ export class ListaLivrosComponent{
   //É uma convensão usar "$" quando temos uma variavel que representa um "Observable"
   //O "switchMap" só faz a requisição ao servidor quando terminarmos de escrever
   //Com "filter", os demais pipes só irão ser chamados quando a condição for válida
+  //O "debounceTime" é um delay para a execução do próximo operador, assim evitamos varias requisiçoes ao servidor ao digitar.
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
     filter((valorDigitado) => valorDigitado.length >= 3),
     switchMap(valorDigitado => this.service.buscar(valorDigitado)),
+    tap(respostaApi => console.log(respostaApi)),
     map(items => this.livrosResultadoParaLivros(items))
   )
 
