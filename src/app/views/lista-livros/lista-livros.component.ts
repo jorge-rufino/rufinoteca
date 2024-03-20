61c4748989716e8b94fe2ecbf9f212a71164faac
jorge-rufino <jorge.rufino1986@gmail.com>
2024-03-20 10:38:54 -0300

    Operador Filter para buscar a partir de 3 caracteres

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { FormControl } from '@angular/forms';
 import { Item } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { switchMap, map } from 'rxjs';
+import { switchMap, map, filter } from 'rxjs';
 import { Livro } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
@@ -19,7 +19,9 @@ export class ListaLivrosComponent{
 
   //É uma convensão usar "$" quando temos uma variavel que representa um "Observable"
   //O "switchMap" só faz a requisição ao servidor quando terminarmos de escrever
+  //Com "filter", os demais pipes só irão ser chamados quando a condição for válida
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    filter((valorDigitado) => valorDigitado.length >= 3),
     switchMap(valorDigitado => this.service.buscar(valorDigitado)),
     map(items => this.livrosResultadoParaLivros(items))
   )
